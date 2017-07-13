b57f108de809fe935e80e63e0aad9fe86fea28d3
Damien Plenard <damien@plenard.me>
2017-07-13 17:21:55 +0200

    enable logging using RUST_LOG env variable

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Repository to learn rust by creating an HTTP API to retrieve you IP.
 ## Quickstart
 
 ```shell
-cargo run
+RUST_LOG=info cargo run
 curl -i 'http://localhost:8080/v1/ip'
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 extern crate iron;
 extern crate mount;
 extern crate router;
+extern crate logger;
+extern crate env_logger;
 
 
-use iron::Iron;
+use iron::prelude::*;
 use mount::Mount;
+use logger::Logger;
 
 
 mod api;
 
 
 fn main() {
+    env_logger::init().unwrap();
+
     let mut middleware = Mount::new();
     middleware.mount("/v1", api::middleware());
-    Iron::new(middleware).http("0.0.0.0:8080").unwrap();
+
+    let mut chain = Chain::new(middleware);
+    let (logger_before, logger_after) = Logger::new(None);
+    chain.link_before(logger_before);
+    chain.link_after(logger_after);
+    Iron::new(chain).http("0.0.0.0:8080").unwrap();
 }
